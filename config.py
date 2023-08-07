c365b56287525cc2b7947210ec8f33f87c044e8b
Henry-yh <61622836+Henry-yh@users.noreply.github.com>
2023-08-07 21:21:15 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
-host = 'http://47.75.82.199:8020'
+host = 'http://00.00.00.000:8020'
 
-username = '18361402318'
-password='123456'
+username = '18311111111'
+password='111111'
 
 token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJsb2dpblR5cGUiOiJBRE1JTiIsImV4cCI6MTY5MjMyOTE1MywidXNlcm5hbWUiOiIxODM2MTQwMjMxOCJ9.ndzHnvgGEE2c4NcFx9egjILJ10tdHkC__Wqh1Z5brYw'
 

98a5ff0a55bac8a3950210b2b2859218688c851c
Henry-yh <61622836+Henry-yh@users.noreply.github.com>
2023-08-07 21:32:33 +0800

    Delete configs directory

--- configs/config.py
+++
@@ -1,7 +0,0 @@
-host = 'http://00.00.00.000:8020'
-
-username = '18311111111'
-password='111111'
-
-token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJsb2dpblR5cGUiOiJBRE1JTiIsImV4cCI6MTY5MjMyOTE1MywidXNlcm5hbWUiOiIxODM2MTQwMjMxOCJ9.ndzHnvgGEE2c4NcFx9egjILJ10tdHkC__Wqh1Z5brYw'
-

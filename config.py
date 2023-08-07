c3e6143576b6475e88a9f407993e39eefd915f13
Henry-yh <61622836+Henry-yh@users.noreply.github.com>
2023-08-07 21:19:06 +0800

    Create config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+host = 'http://47.75.82.199:8020'
+
+username = '18361402318'
+password='123456'
+
+token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJsb2dpblR5cGUiOiJBRE1JTiIsImV4cCI6MTY5MjMyOTE1MywidXNlcm5hbWUiOiIxODM2MTQwMjMxOCJ9.ndzHnvgGEE2c4NcFx9egjILJ10tdHkC__Wqh1Z5brYw'
+
